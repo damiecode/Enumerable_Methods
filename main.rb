e94d8899685f27ff02d9facbbf435294d6c0c0e5
Damie <damidigi@yahoo.com>
2019-10-14 16:51:40 +0100

    Modify Methods
    Signed-off-by: Ale Damilola <damidigi@yahoo.com>

--- main.rb
+++ main.rb
@@ -30,48 +30,21 @@ module Enumerable
     item_select
   end
 
-  def my_all?(param = nil)
-    if block_given?
-      my_each { |i| return false unless yield(i) }
-    elsif param.class == Class
-      my_each { |i| return false unless i.class == param }
-    elsif param.class == Regexp
-      my_each { |i| return false unless i =~ param }
-    elsif param.nil?
-      my_each { |i| return false unless i }
-    else
-      my_each { |i| return false unless i == param }
-    end
+  def my_all?
+    return my_each { |i| return false if yield(i) == false } unless block_given?
+
     true
   end
 
-  def my_any?(param = nil)
-    if block_given?
-      my_each { |i| return true if yield(i) }
-    elsif param.class == Class
-      my_each { |i| return true if i.class == param }
-    elsif param.class == Regexp
-      my_each { |i| return true if i =~ param }
-    elsif param.nil?
-      my_each { |i| return true if i }
-    else
-      my_each { |i| return true if i == param }
-    end
+  def my_any?
+    return my_each { |i| return true if yield(i) } unless block_given?
+
     false
   end
 
-  def my_none?(param = nil)
-    if block_given?
-      my_each { |i| return false if yield(i) }
-    elsif param.class == Class
-      my_each { |i| return false if i.class == param }
-    elsif param.class == Regexp
-      my_each { |i| return false if i =~ param }
-    elsif param.nil?
-      my_each { |i| return false if i }
-    else
-      my_each { |i| return false if i == param }
-    end
+  def my_none?
+    return my_each { |i| return false if yield(i) } unless block_given?
+
     true
   end
 
@@ -88,7 +61,7 @@ module Enumerable
   end
 
   def my_map(arg = nil)
-    return :my_map unless block_given?
+    return to_enum unless block_given?
 
     arr = []
     my_each do |i|
@@ -101,28 +74,12 @@ module Enumerable
     arr
   end
 
-  def my_inject(*args)
-    arr = to_a.dup
-    if args[0].nil?
-      operand = arr.shift
-    elsif args[1].nil? && !block_given?
-      symbol = args[0]
-      operand = arr.shift
-    elsif args[1].nil? && block_given?
-      operand = args[0]
-    else
-      symbol = args[0]
-      operand = args[1]
+  def my_inject(value = self[0])
+    value ||= []
+    my_each do |element|
+      value = yield(value, element)
     end
-
-    arr[0..-1].my_each do |i|
-      operand = if symbol
-                  operand.send(symbol, i)
-                else
-                  yield(operand, i)
-                end
-    end
-    operand
+    value
   end
 end
 
