d45f9e626a4e086525b50520f5a30cb2c786a522
Ale Damilola <damidigi@yahoo.com>
2019-10-26 22:26:03 +0100

    Add tests for methods

---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require '../main'
+RSpec.describe Enumerable do
+  let(:arr) { [4, 5, 8, 2, 1, 9, 3, 7, 6] }
+  let(:new_arr) { [] }
+
+  describe '#my_each' do
+    let(:new_arr_each) { [] }
+    it 'iterates through array of elements' do
+      my_array = []
+      original_array = []
+      arr.my_each { |i| my_array << i * 2 }
+      arr.each { |i| original_array << i * 2 }
+      expect(my_array).to eql(original_array)
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'returns indexes of elements in an array' do
+      %w[e nu me ra ble].my_each_with_index { |_elem, index| new_arr << index }
+      expect(new_arr).to eql([0, 1, 2, 3, 4])
+    end
+  end
+
+  describe '#my_count' do
+    it 'returns the counts of elements that evaluate to be true' do
+      expect(arr.my_count { |i| i < 7 }).to eql(6)
+    end
+  end
+
+  describe '#my_map' do
+    it 'returns a new array with the results of running through the block once on a given array' do
+      result = arr.my_map { |i| i**2 }
+      expect(result).to eql(arr.map { |i| i**2 })
+    end
+  end
+
+  describe '#my_none' do
+    it 'returns true if none of the element meet the criteria in the block' do
+      expect(arr.my_none? { |num| num == 10 }).to eql(true)
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns selected values in an array based on the given block' do
+      expect(arr.my_select(&:odd?)).to eql([5, 1, 9, 3, 7])
+    end
+  end
+end
